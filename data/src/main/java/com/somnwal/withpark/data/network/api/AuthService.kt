e9f133af772906cb9568169c694b28fefd4e7ee8
솜놜 <90139018+somnwal@users.noreply.github.com>
2025-05-30 21:55:30 +0900

    22

--- data/src/main/java/com/somnwal/withpark/data/network/api/AuthService.kt
+++ data/src/main/java/com/somnwal/withpark/data/network/api/AuthService.kt
@@ -11,9 +11,9 @@ internal interface AuthService {
     @POST("/api/v1/auth/login/kakao")
     suspend fun kakaoLogin(@Body request: LoginRequest): LoginResponse
 
-    @POST("/api/vi/auth/logout")
+    @POST("/api/v1/auth/logout")
     suspend fun logout()
 
-    @POST("/app/papi/v1/auth/refresh")
+    @POST("/api/v1/auth/refresh")
     suspend fun refreshToken(@Body refreshToken: RefreshTokenRequest): RefreshTokenResponse
 }
--- domain/src/main/java/com/somnwal/withpark/domain/response/auth/RefreshTokenResponse.kt
+++ domain/src/main/java/com/somnwal/withpark/domain/response/auth/RefreshTokenResponse.kt
@@ -1,6 +1,5 @@
 package com.somnwal.withpark.domain.response.auth
 
-import com.somnwal.withpark.domain.model.auth.RefreshToken
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
